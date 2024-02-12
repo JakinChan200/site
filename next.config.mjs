0fe6b58d2ef6a290bd92916cd2bd84c8e936c609
Jakin Chan <JakinChan200@gmail.com>
2024-02-11 21:48:59 -0800

    Continue reverting

--- next.config.mjs
+++ next.config.mjs
@@ -1,46 +1,46 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: "",
-    unoptimized: true,
-  },
-  basePath: "",
-  assetPrefix: "",
-};
-
-export default nextConfig;
-
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//     /**
-//      * Enable static exports for the App Router.
-//      *
-//      * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-//      */
-//     output: "export",
+//   output: "export",
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     loader: "akamai",
+//     path: "",
+//     unoptimized: true,
+//   },
+//   basePath: "",
+//   assetPrefix: "",
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: "export",
   
-//     /**
-//      * Set base path. This is the slug of your GitHub repository.
-//      *
-//      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-//      */
-//     basePath: "/site",
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/site",
   
-//     /**
-//      * Disable server-based image optimization. Next.js does not support
-//      * dynamic features with static exports.
-//      *
-//      * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-//      */
-//     images: {
-//       unoptimized: true,
-//     },
-//   };
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+      unoptimized: true,
+    },
+  };
   
 //   export default nextConfig;
 
