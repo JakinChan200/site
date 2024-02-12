86bdb13dc40178ea1a4d8f26f0be3fc796ed3d89
Jakin Chan <JakinChan200@gmail.com>
2024-02-11 23:55:55 -0800

    Trying again

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ import Link from 'next/link';
 // import githubLogo from "../public/assets/GithubLogo.png";
 // import emailLogo from "../public/assets/EmailLogo.png";
 
-// import profilePicture from "/site/assets/Jakin.webp";
-// import linkedinLogo from "/site/assets/LinkedinLogo.png";
-// import githubLogo from "/site/assets/GithubLogo.png";
-// import emailLogo from "/site/assets/EmailLogo.png";
+import profilePicture from "/site/assets/Jakin.webp";
+import linkedinLogo from "/site/assets/LinkedinLogo.png";
+import githubLogo from "/site/assets/GithubLogo.png";
+import emailLogo from "/site/assets/EmailLogo.png";
 
 
 
@@ -41,7 +41,7 @@ export default function Home() {
                 <button className="transition hover:-translate-y-2">
                   <Image
                     className="rounded-full shadow-lg bg-transparent mr-3"
-                    src="/site/assets/LinkedinLogo.png"
+                    src={linkedinLogo}
                     alt="LinkedIn Logo"
                     width={50}
                     height={50}
@@ -53,7 +53,7 @@ export default function Home() {
                 <button className="transition hover:-translate-y-2">
                   <Image
                     className="rounded-full shadow-lg bg-transparent mr-3"
-                    src="/site/assets/GithubLogo.png"
+                    src={githubLogo}
                     alt="Github Logo"
                     width={50}
                     height={50}
@@ -65,7 +65,7 @@ export default function Home() {
                 <button className="transition hover:-translate-y-2">
                   <Image
                     className="rounded-full shadow-lg bg-transparent mr-3"
-                    src="/site/assets/EmailLogo.png"
+                    src={emailLogo}
                     alt="EmailLogo"
                     width={50}
                     height={50}
@@ -77,7 +77,7 @@ export default function Home() {
           </div>
         <div className="flex h-screen justify-center items-center">
           <Image
-            src="/site/assets/Jakin.webp"
+            src={profilePicture}
             width={500}
             height={500}
             alt="jakin image"
@@ -205,4 +205,3 @@ export default function Home() {
     </main>
   );
 }
-
--- next.config.mjs
+++ next.config.mjs
@@ -1,46 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-    output: "export",
-  
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/site",
-  
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     loader: "akamai",
-//     path: "",
-//     unoptimized: true,
-//   },
-//   basePath: "",
-//   assetPrefix: "",
-// };
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
 
-// export default nextConfig;
-//   export default nextConfig;
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/site",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {};
