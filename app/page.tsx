2630d9d93d9323295630a2878b1a2fa04af467b4
Jakin Chan <JakinChan200@gmail.com>
2024-02-14 21:01:52 -0800

    Changed image paths to links

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
                 <button className="transition hover:-translate-y-2">
                   <Image
                     className="rounded-full shadow-lg bg-transparent mr-4"
-                    src="/site/assets/LinkedinLogo.png"
+                    src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550401302237204/LinkedinLogo.png?ex=65e00df2&is=65cd98f2&hm=9840ee7eb8866bfebba2a8b82ae2e784339dcad8e36c4d27513b8dafaa6edac6&"
                     alt="LinkedIn Logo"
                     width={50}
                     height={50}
@@ -63,7 +63,7 @@ export default function Home() {
                 <button className="transition hover:-translate-y-2">
                   <Image
                     className="rounded-full shadow-lg bg-transparent mr-4"
-                    src="/site/assets/GithubLogo.png"
+                    src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550083311206460/GithubLogo.png?ex=65e00da7&is=65cd98a7&hm=40151a9cb075a74b4261bd2292b14536b9e316926ab6b3113b838753e89b32aa&"
                     alt="Github Logo"
                     width={50}
                     height={50}
@@ -75,7 +75,7 @@ export default function Home() {
                 <button className="transition hover:-translate-y-2">
                   <Image
                     className="rounded-full shadow-lg bg-transparent mr-4"
-                    src="/site/assets/EmailLogo.png"
+                    src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550083072008212/EmailLogo.png?ex=65e00da7&is=65cd98a7&hm=65d8abfe2aba4fa693bd1bb750530c37c3d6abb49ce52b24a3838c8129ee2332&"
                     alt="EmailLogo"
                     width={50}
                     height={50}
@@ -87,7 +87,7 @@ export default function Home() {
                 <button className="transition hover:-translate-y-2">
                   <Image
                     className="rounded-full shadow-lg bg-transparent mr-4"
-                    src="https://cdn.discordapp.com/attachments/1112541604318822600/1113948933572534314/174855.png?ex=65d7c2c9&is=65c54dc9&hm=dcf3c84d433d42a30d69e8a942e8c4cc1d1caa2859a8b9a7954703e819fae621&"
+                    src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550017007657010/InstagramLogo.png?ex=65e00d97&is=65cd9897&hm=a618f79cb21bb005c050bcb5b5df69e4587968e91b959e116c0a1c4704168446&"
                     alt="InstagramLogo"
                     width={50}
                     height={50}
@@ -99,7 +99,7 @@ export default function Home() {
           </div>
           <div className="flex order-2 h-screen justify-center items-center">
             <Image
-              src="/site/assets/Jakin.webp"
+              src="https://cdn.discordapp.com/attachments/1207549973063794700/1207551334262374400/jakin.webp?ex=65e00ed1&is=65cd99d1&hm=2e7423384a1bc94f693b5bca415402c604e0e710f9ecf81cd2f09ba59cd0c23d&"
               width={500}
               height={500}
               alt="jakin image"
@@ -116,7 +116,7 @@ export default function Home() {
           <div className="self-center m-2">
             <Image
               className="rounded-lg"
-              src="/assets/UCR.jpg"
+              src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550016802267136/UCR.jpg?ex=65e00d97&is=65cd9897&hm=84c51cfcd1dec33b5dac9df851fc2b3388b625fe03f2eb1b56c8ffcda7c0208a&"
               width={250}
               height={250}
               alt="jakin image"
@@ -145,7 +145,7 @@ export default function Home() {
           <div className="self-center m-2">
             <Image
               className="rounded-lg"
-              src="/assets/UCR.jpg"
+              src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550016802267136/UCR.jpg?ex=65e00d97&is=65cd9897&hm=84c51cfcd1dec33b5dac9df851fc2b3388b625fe03f2eb1b56c8ffcda7c0208a&"
               width={250}
               height={250}
               alt="jakin image"
