7adf0aadefae3a0d1390eca12dcbe30b8249bff8
Jakin Chan <JakinChan200@gmail.com>
2024-02-23 02:51:58 -0800

    centering stuff

--- public/pages/education.tsx
+++ public/pages/education.tsx
@@ -7,98 +7,100 @@ import { Tag } from  "../components/tag";
 
 export const Education: React.FC= () => {
   return (
-    <div className=" h-screen">
-        <div className="w-full self-center text-center font-extrabold text-4xl text-black">
-        Education
-        </div>
-        <div className="flex justify-center mb-4">
-            <div className="perspective-1000 group cursor-pointer h-40 w-1/2 items-center"> {/*Start*/}
-                <div className="relative transform-style-3d transition-transform duration-1000 transform group-hover:rotate-y-180">
-                    <div className="backface-hidden absolute w-full h-full rotate-y-0 "> {/*End*/}
-                        <div className="flex flex-none flex-wrap rounded-lg display:block w-full shadow-xl h-40 bg-white border-blue-600 border-x-2">
-                            <div className="self-center m-2">
-                                <Image
-                                className="rounded-lg"
-                                src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550016802267136/UCR.jpg?ex=65e00d97&is=65cd9897&hm=84c51cfcd1dec33b5dac9df851fc2b3388b625fe03f2eb1b56c8ffcda7c0208a&"
-                                width={250}
-                                height={250}
-                                alt="jakin image"
-                                priority
-                                />
-                            </div>
-                            <div className="flex flex-wrap flex-col text-sky-600 *:ml-2">
-                                <div className="font-bold sans-serif text-black w-full mt-2">
-                                University of California, Riverside
+    <div className=" h-screen w-screen flex items-center">
+        <div className= "w-screen">
+            <div className="w-full self-center text-center font-extrabold text-4xl text-black">
+            Education
+            </div>
+            <div className="flex justify-center mb-4">
+                <div className="perspective-1000 group cursor-pointer h-40 w-1/2 items-center"> {/*Start*/}
+                    <div className="relative transform-style-3d transition-transform duration-1000 transform group-hover:rotate-y-180">
+                        <div className="backface-hidden absolute w-full h-full rotate-y-0 "> {/*End*/}
+                            <div className="flex flex-none flex-wrap rounded-lg display:block w-full shadow-xl h-40 bg-white border-blue-600 border-x-2">
+                                <div className="self-center m-2">
+                                    <Image
+                                    className="rounded-lg"
+                                    src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550016802267136/UCR.jpg?ex=65e00d97&is=65cd9897&hm=84c51cfcd1dec33b5dac9df851fc2b3388b625fe03f2eb1b56c8ffcda7c0208a&"
+                                    width={250}
+                                    height={250}
+                                    alt="jakin image"
+                                    priority
+                                    />
                                 </div>
-                                <div>
-                                MS in Compututer Science
-                                </div>
-                                <div>
-                                March 2024
+                                <div className="flex flex-wrap flex-col text-sky-600 *:ml-2">
+                                    <div className="font-bold sans-serif text-black w-full mt-2">
+                                    University of California, Riverside
+                                    </div>
+                                    <div>
+                                    MS in Compututer Science
+                                    </div>
+                                    <div>
+                                    March 2024
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="backface-hidden absolute w-full h-40 rotate-y-180">
-                        <div className="flex flex-none flex-wrap rounded-lg bg-white display:block w-full shadow-xl border-blue-600 border-x-2">
-                            <div className="self-center  m-2">
-                                <div className="text-black ml-2 font-bold">
-                                Relevant Coursework:
-                                </div>
-                                <div className="ml-1 mb-2">
-                                <Tag text="Advanced Computer Architecture"/>
-                                <Tag text="Advanced Software Testing and Analysis"/> 
-                                <Tag text="Big Data Management"/>
-                                <Tag text="Database Management Systems"/>
-                                <Tag text="Data Mining Techniques"/>
-                                <Tag text="Design and Analysis of Algorithms"/>  
-                                <Tag text="Fundamentals of Machine Learning"/>
+                        <div className="backface-hidden absolute w-full h-40 rotate-y-180">
+                            <div className="flex flex-none flex-wrap rounded-lg bg-white display:block w-full shadow-xl border-blue-600 border-x-2">
+                                <div className="self-center  m-2">
+                                    <div className="text-black ml-2 font-bold">
+                                    Relevant Coursework:
+                                    </div>
+                                    <div className="ml-1 mb-2">
+                                    <Tag text="Advanced Computer Architecture"/>
+                                    <Tag text="Advanced Software Testing and Analysis"/> 
+                                    <Tag text="Big Data Management"/>
+                                    <Tag text="Database Management Systems"/>
+                                    <Tag text="Data Mining Techniques"/>
+                                    <Tag text="Design and Analysis of Algorithms"/>  
+                                    <Tag text="Fundamentals of Machine Learning"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="flex justify-center mt-8">
-            <div className="perspective-1000 group cursor-pointer h-40 w-1/2 items-center"> {/*Start*/}
-                <div className="relative transform-style-3d transition-transform duration-1000 transform group-hover:rotate-y-180">
-                    <div className="backface-hidden absolute w-full h-full rotate-y-0 "> {/*End*/}
-                        <div className="flex flex-none flex-wrap rounded-lg bg-white display:block w-full shadow-xl border-blue-600 border-x-2">
-                            <div className="self-center m-2 ">
-                                <Image
-                                className="rounded-lg"
-                                src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550016802267136/UCR.jpg?ex=65e00d97&is=65cd9897&hm=84c51cfcd1dec33b5dac9df851fc2b3388b625fe03f2eb1b56c8ffcda7c0208a&"
-                                width={250}
-                                height={250}
-                                alt="jakin image"
-                                priority
-                                />
-                            </div>
-                            <div className="flex flex-wrap flex-col text-sky-600 *:ml-2">
-                                <div className="font-bold sans-serif text-black w-full mt-2">
-                                University of California, Riverside
+            <div className="flex justify-center mt-8">
+                <div className="perspective-1000 group cursor-pointer h-40 w-1/2 items-center"> {/*Start*/}
+                    <div className="relative transform-style-3d transition-transform duration-1000 transform group-hover:rotate-y-180">
+                        <div className="backface-hidden absolute w-full h-full rotate-y-0 "> {/*End*/}
+                            <div className="flex flex-none flex-wrap rounded-lg bg-white display:block w-full shadow-xl border-blue-600 border-x-2">
+                                <div className="self-center m-2 ">
+                                    <Image
+                                    className="rounded-lg"
+                                    src="https://cdn.discordapp.com/attachments/1207549973063794700/1207550016802267136/UCR.jpg?ex=65e00d97&is=65cd9897&hm=84c51cfcd1dec33b5dac9df851fc2b3388b625fe03f2eb1b56c8ffcda7c0208a&"
+                                    width={250}
+                                    height={250}
+                                    alt="jakin image"
+                                    priority
+                                    />
                                 </div>
-                                <div>
-                                BS in Compututer Science
-                                </div>
-                                <div>
-                                March 2023
+                                <div className="flex flex-wrap flex-col text-sky-600 *:ml-2">
+                                    <div className="font-bold sans-serif text-black w-full mt-2">
+                                    University of California, Riverside
+                                    </div>
+                                    <div>
+                                    BS in Compututer Science
+                                    </div>
+                                    <div>
+                                    March 2023
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="backface-hidden absolute w-full h-40 rotate-y-180">
-                        <div className="flex flex-none flex-wrap rounded-lg bg-white display:block w-full shadow-xl h-40 border-blue-600 border-x-2">
-                            <div className="self-start m-2">
-                                <div className="text-black ml-2 font-bold">
-                                Relevant Coursework:
-                                </div>
-                                <div className=" ml-1 mb-2">
-                                <Tag text="Algorithm Engineering"/>
-                                <Tag text="Concurrent Programming and Parallel Systems"/>
-                                <Tag text="Database Management Systems"/>
-                                <Tag text="Artificial Intelligence"/>
+                        <div className="backface-hidden absolute w-full h-40 rotate-y-180">
+                            <div className="flex flex-none flex-wrap rounded-lg bg-white display:block w-full shadow-xl h-40 border-blue-600 border-x-2">
+                                <div className="self-start m-2">
+                                    <div className="text-black ml-2 font-bold">
+                                    Relevant Coursework:
+                                    </div>
+                                    <div className=" ml-1 mb-2">
+                                    <Tag text="Algorithm Engineering"/>
+                                    <Tag text="Concurrent Programming and Parallel Systems"/>
+                                    <Tag text="Database Management Systems"/>
+                                    <Tag text="Artificial Intelligence"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- public/pages/experience.tsx
+++ public/pages/experience.tsx
@@ -4,10 +4,49 @@ import React from "react";
 import Link from 'next/link';
 import Image from "next/image";
 
+
+import { useEffect, useState } from "react";
+
+const tracks = ["Activities Leader", "Operations Lead", "Resident Advisor", "GWC Volunteer"];
+
+const ScrollingText = () => {
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrent((curr) => (curr === 2 ? 0 : curr + 1));
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="text-blue-400 w-full text-right font-bold -mb-10 md:-mb-20">
+      <p className="text-xl md:text-3xl mb-0 text-black">Experience:</p>
+      <div className="h-10 md:h-20 overflow-hidden text-2xl md:text-7xl">
+        {tracks.map((track, index) => (
+          <div
+            key={index}
+            className="duration-500 h-10 md:h-20"
+            style={{
+              transform: `translateY(${-100 * current}%)`,
+            }}
+          >
+            {track}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export const Experience: React.FC= () => {
   return (
-    <div className=" h-screen">
-        Bruh
+    <div className=" h-screen flex items-center justify-center">
+      <div className="">
+        {/* <div className="w-full self-center text-center font-extrabold text-4xl text-black">
+          Experience
+        </div> */}
+        <ScrollingText/>
+      </div>
     </div>
   );
 };
